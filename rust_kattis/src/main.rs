b54352ea26055cc48fe9a952037951856b58d667
DreamerChaserHAH <109950820+DreamerChaserHAH@users.noreply.github.com>
2023-02-09 09:03:23 +0800

    Commit #5
    Conclusion for this problem

--- rust_kattis/src/main.rs
+++ rust_kattis/src/main.rs
@@ -4,6 +4,21 @@
 </summary>
 */
 
+/*#region Importing Necessary Libraries */
+use std::io;
+/*#endregion */
 fn main() {
-    println!("Hello World!");
+
+    /*#region init */
+    let mut input_string = String::new();
+    let input = io::stdin();
+    let numbers: Vec<i32> = Vec::new();
+    /*endregion */
+
+    /*#region input */
+    input.read_line(&mut input_string);
+    let numbers = input_string.trim().split_whitespace().map(|x| x.parse::<i32>().unwrap()).collect::<Vec<i32>>();
+
+    println!("{}", numbers[0] * numbers[1] * numbers[2]);
+    /*#endregion */
 }
